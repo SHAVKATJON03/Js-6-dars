8dc1612e6b64eb0127593e874dc308e03722543a
SHAVKATJON03 <shavkatjonallayorov@gmail.com>
2023-03-05 11:14:43 +0500

    delete input java script

--- main.js
+++ main.js
@@ -1,67 +1,69 @@
-// function kitten(str, ind) {
-//   let s = "";
-//   for (let i = 0; i < str.length; i++) {
-//     if (i !== ind) {
-//       s=s+str[i];
-//     }
-//     // console.log(str[i]);
-//   }
-//   return s;
-// }
-// console.log(kitten("webp", 2));
-
-
-
-// const form = document.getElementsByClassName("formbtn");
-// const input = document.getElementById("input");
-// const list = document.getElementById("list");
-// const todoApptext = [];
-// function createHtmlElemenets(e) {
-
-//   e.preventDefault();
-
-//   todoApptext.push(input.value);
-
-//   console.log(todoApptext);
-
-//   let resulut = "";
-
-//   todoApptext.forEach((item, index) => {
-//     resulut = resulut + `<li>${index + 1} ${item}</li>`;
-//   });
-//   list.innerHTML = resulut;
-// }
-// form.addEventListener("submit",createHtmlElemenets);
-
-
-
-
-
-
-
-
-
-
-const form = document.getElementById("form")
+const form = document.getElementById("form");
 const input = document.getElementById("input");
-const list =document.getElementById("list")
-const todoApptext = [];
+const list = document.getElementById("list");
+let todoApptext = [];
+let index=0
 function createHtmlElemenets(e) {
-
   e.preventDefault();
 
-  todoApptext.push(input.value);
-
-//   console.log(todoApptext);
-
-  let resulut = "";
+  todoApptext.push({
+    text:input.value,
+    id: index
+  });
+  index++
+  let result = "";
 
   todoApptext.forEach((item, index) => {
-    resulut = resulut + `<li class="item"> ${item} 
+    result = result + `<li class="item"> ${item.text}
 
     <img class="krug" src="./img/green_krug.svg" alt="icon">
-    <img class=" icon" src="./img/delete.svg" alt="icon"> </li>`;
+    <img class=" icon" src="./img/delete.svg" onclick="removeTodoApp(${item.id})" alt="icon"> </li>`;
   });
-  list.innerHTML = resulut;
+
+  // const li = document.createElement("li");
+  // li.setAttribute("class", "item");
+  // let listContent = document.createElement("div");
+  // todoApptext.forEach((item) => {
+
+  //   const li = document.createElement("li");
+  //   li.setAttribute("class", "item");
+  //   const img = document.createElement("img");
+  //   img.setAttribute("src", "./img/delete.svg");
+  //   img.setAttribute("class", "icon");
+
+  //   const imgSvg = document.createElement("img");
+  //   imgSvg.setAttribute("src", "./img/green_krug.svg");
+  //   imgSvg.setAttribute("class", "krug");
+
+
+  //   const p = document.createElement("p");
+  //   p.textContent = item;
+  //   li.appendChild(p);
+  //   li.appendChild(img);
+  //   li.appendChild(imgSvg)
+  //   console.log(li);
+  //   // result += li;
+  //   listContent.appendChild(li);
+  // });
+  // console.log(listContent)
+  // list.replaceChildren(listContent);
+  list.innerHTML = result;
 }
 form.addEventListener("submit", createHtmlElemenets);
+
+
+function removeTodoApp(id){  
+todoApptext = todoApptext.filter((item)=> item.id !== id);
+
+
+let result = "";
+
+todoApptext.forEach((item, index) => {
+  result = result + `<li class="item"> ${item.text}
+
+  <img class="krug" src="./img/green_krug.svg" alt="icon">
+  <img class=" icon" src="./img/delete.svg" onclick="removeTodoApp(${item.id})" alt="icon"> </li>`;
+});
+
+list.innerHTML = result;
+}
